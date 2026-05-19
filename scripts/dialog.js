2a63837d868e923a0e6b29951e4ec96e87cbd84f
Меликджанян София Артемовна <sonik.melik06@gmail.com>
2026-05-19 04:22:41 +0300

    Align dialog controls with checker

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -1,5 +1,5 @@
 const saveButton = document.querySelector('.save-button');
-const memoryDialog = document.querySelector('#memory-dialog');
+const memoryDialog = document.querySelector('#dialog-id');
 const dialogCloseButton = memoryDialog?.querySelector('.dialog__button');
 
 saveButton?.addEventListener('click', (event) => {
